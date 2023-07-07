dd1f9c876c586339df47b09de80006db294536ff
KongZ <goonohc@gmail.com>
2023-07-07 10:43:04 +0700

    recompile wiht 5.1.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const { PluginWebpackConfig } = require('graylog-web-plugin');
 const { loadBuildConfig } = require('graylog-web-plugin');
 const path = require('path');
+const buildConfig = loadBuildConfig(path.resolve(__dirname, './build.config'));
 
 // Remember to use the same name here and in `getUniqueId()` in the java MetaData class
-module.exports = new PluginWebpackConfig(__dirname, 'com.kongz.graylog.plugins.slack.SlackNotificationPlugin', loadBuildConfig(path.resolve(__dirname, './build.config')), {
+module.exports = new PluginWebpackConfig(__dirname, 'com.kongz.graylog.plugins.slack.SlackNotificationPlugin', buildConfig, {
   // Here goes your additional webpack configuration.
 });
